23d9b4844042169c5d1c07d33d87385c10a0fc1a
Jonathan Hartman <j@p4nt5.com>
2016-09-21 10:02:06 -0700

    Modify Python path instead of import statements
    There are other imports further down the line that'll still fail.

--- files/default/fpm-recipes/okta-openvpn/recipe.rb
+++ files/default/fpm-recipes/okta-openvpn/recipe.rb
@@ -59,9 +59,12 @@ class OktaOpenvpn < FPM::Cookery::Recipe
   #
   def build
     inline_replace 'okta_openvpn.py' do |s|
-      %w(M2Crypto urllib3 certifi).each do |m|
-        s.gsub!(/^import #{m}$/, "from okta_openvpn import #{m}")
-      end
+      s.gsub!(
+        /^import ConfigParser$/,
+        "import sys\n" \
+        "sys.path.append(\"/usr/lib/openvpn/plugins/okta_openvpn\")\n" \
+        'import ConfigParser'
+      )
     end
     make
   end
